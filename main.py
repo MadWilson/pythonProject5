55ea4b7e34165e543ed58a356b6b63424eae116d
Филиппов Сергей <filippovsergej@iMac-Mac009.local>
2021-03-05 09:37:37 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+#19
+
+'''
+def function(array):
+    finaly_array = []
+
+'''
+
+
+#27
+
+a = int(input())
+b = int(input())
+c = int(input())
+def my_function:
+   while a != 0 and b != 0 and c != 0:
+     if a > b:
+        a = a % b
+     else:
+        b = b % a
+
+
+print(a + b)
+
+#31
+
+
+
+#35
+
+
+'''
+def lines(n):
+    return '*' * n
+lines()
+
+'''
